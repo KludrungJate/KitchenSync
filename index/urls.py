354ef121d477fe88c4c200306aa54aa7a409bce6
Jatenadu <aphisit17082546@gmail.com>
2025-08-12 16:24:28 +0700

    เพิ่มผุ่มลบแย้ววว

--- index/urls.py
+++ index/urls.py
@@ -3,4 +3,5 @@ from index import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('delete/<int:ingredient_id>/', views.delete_ingredient, name='delete_ingredient'),
 ]
--- index/views.py
+++ index/views.py
@@ -1,8 +1,13 @@
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Ingredient
 
 def index(request):
     ingredients = Ingredient.objects.all().order_by('prepared_date')
     latest_ingredients = Ingredient.objects.order_by('-created_at')[:4]  # 4 รายการล่าสุด
     return render(request, 'main.html', {'ingredients': ingredients, 'latest_ingredients': latest_ingredients})
+
+def delete_ingredient(request, ingredient_id):
+    if request.method == "POST":
+        ing = get_object_or_404(Ingredient, id=ingredient_id)
+        ing.delete()
+    return redirect('index')
